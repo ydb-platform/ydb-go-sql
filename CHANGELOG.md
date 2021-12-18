1234a66d009a2d9371774591d0af48984067ebf0
asmyasnikov <79263256394@ya.ru>
2021-12-18 21:08:49 +0300

    add CHANGELOG.md + version

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+## 0.0.1
+* Implements database/sql driver
+* Supports scheme, scan and data queries, explain
---
+++ version.go
@@ -0,0 +1,11 @@
+package ydb
+
+import (
+	"google.golang.org/grpc"
+
+	"github.com/ydb-platform/ydb-go-sdk/v3"
+)
+
+const Version = "ydb-go-sql/0.0.1"
+
+const Versions = Version + "," + ydb.Version + ",grpc/" + grpc.Version
